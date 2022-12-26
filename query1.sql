fcc834feb997c1363a7dae4cc1844d4d7a5080e9
sauravnegih <52993407+sauravnegih@users.noreply.github.com>
2022-12-26 08:57:18 +0530

    Add files via upload

---
+++ query1.sql
@@ -0,0 +1,17 @@
+create database bankmanagementsystem;
+show databases;
+use bankmanagementsystem;
+create table signup(formno varchar(20),name varchar(20),father_name varchar(20),dob varchar(20),gender varchar(20),email varchar(30),maritial_status varchar(20),address varchar(50),city varchar(20),pin varchar(20),state varchar(20));
+show tables;
+select * from signup;
+create table signuptwo(formno varchar(20),religion varchar(20),category varchar(20),income varchar(20),education varchar(20),pan varchar(20),aadhar varchar(20),senior_citizen varchar(20),existing_account varchar(20));
+show tables;
+select * from signuptwo;
+create table signupthree(formno varchar(20),type varchar(30),card_number varchar(30),pin_number varchar(20),facility varchar(100));
+select * from signupthree;
+create table login(formno varchar(20),cardnumber varchar(40),pinnumber varchar(50));
+select * from login;
+drop table bank;
+create table bank(pinnumber varchar(2000),date varchar(2000),ype varchar(2000),amount varchar(2000));
+select * from bank;
+drop database bankmanagementsystem;
